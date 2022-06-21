dba68ad1970c4bf24a73bf73f764ad4d47aa80d3
Artsiom Holikau <artsiom@dfinance.co>
2022-06-21 13:54:28 -0400

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-echo ${LOCAL_CERT} > localhost.pem
-echo ${LOCAL_KEY} > localhost-key.pem
+echo "${LOCAL_CERT}" > localhost.pem
+echo "${LOCAL_KEY}" > localhost-key.pem
 
 grpcwebproxy --run_tls_server=true --server_tls_cert_file=localhost.pem --server_tls_key_file=localhost-key.pem --backend_addr=167.172.180.70:9290 --backend_tls=false --allow_all_origins
