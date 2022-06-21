a745938f23ffc2ff76a4f9236038c464cbcf6372
Artsiom Holikau <artsiom@dfinance.co>
2022-06-21 14:24:54 -0400

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ set -ex
 echo "${LOCAL_CERT}" > localhost.pem
 echo "${LOCAL_KEY}" > localhost-key.pem
 
-grpcwebproxy --run_tls_server=true --server_tls_cert_file=localhost.pem --server_tls_key_file=localhost-key.pem --backend_addr=167.172.180.70:9290 --backend_tls=false --allow_all_origins --server_http_debug_port=80
+grpcwebproxy --run_tls_server=true --server_tls_cert_file=localhost.pem --server_tls_key_file=localhost-key.pem --backend_addr=167.172.180.70:9290 --backend_tls=false --allow_all_origins --server_http_debug_port="${PORT}"
